f5d26ef1742a6eddcfc031ae254bc47bd489cc4e
Jose Mayol <mayol.jose.f@gmail.com>
2017-10-28 00:40:16 -0300

    Cambia la busqueda de los renglones de CC al repository responsable.

--- src/main/java/sic/service/IRenglonCuentaCorrienteService.java
+++ src/main/java/sic/service/IRenglonCuentaCorrienteService.java
@@ -1,5 +1,7 @@
 package sic.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import sic.modelo.FacturaVenta;
 import sic.modelo.Nota;
 import sic.modelo.Pago;
@@ -14,5 +16,7 @@ public interface IRenglonCuentaCorrienteService {
     RenglonCuentaCorriente getRenglonCuentaCorrienteDeNota(Nota n, boolean eliminado);
     
     RenglonCuentaCorriente getRenglonCuentaCorrienteDePago(Pago p, boolean eliminado);
+    
+    Page<RenglonCuentaCorriente> getRenglonesCuentaCorriente(long idCuentaCorriente, Pageable page);
     
 }
--- src/main/java/sic/service/impl/RenglonCuentaCorrienteServiceImpl.java
+++ src/main/java/sic/service/impl/RenglonCuentaCorrienteServiceImpl.java
@@ -2,6 +2,8 @@ package sic.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import sic.modelo.FacturaVenta;
 import sic.modelo.Nota;
@@ -39,6 +41,11 @@ public class RenglonCuentaCorrienteServiceImpl implements IRenglonCuentaCorrient
     @Override
     public RenglonCuentaCorriente getRenglonCuentaCorrienteDePago(Pago p, boolean eliminado) {
         return renglonCuentaCorrienteRepository.findByPagoAndEliminado(p, eliminado);
+    }
+    
+    @Override
+    public Page<RenglonCuentaCorriente> getRenglonesCuentaCorriente(long idCuentaCorriente, Pageable page) {
+        return renglonCuentaCorrienteRepository.getRenglonesCuentaCorriente(idCuentaCorriente, page);
     }
     
 }
