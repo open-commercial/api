1bf009322e515a10fb9e8b0e04cbcd664cda279a
Jose Francisco Mayol <mayol.jose.f@gmail.com>
2017-11-06 22:55:39 -0300

    Corrige nombres en el procedure.

--- scripts/migracionConProcedures.sql
+++ scripts/migracionConProcedures.sql
@@ -5,12 +5,12 @@ DELIMITER $$
  DECLARE x  INT;
  
  SET x = 0 , @idPaisInc = (SELECT max(id_Pais) FROM sic.pais) + 1,
- @contador = (SELECT count(*) FROM tabla.pais);
+ @contador = (SELECT count(*) FROM minorista.pais);
  
  WHILE x  < @contador DO
    INSERT INTO sic.pais (id_Pais, eliminado, nombre) values ();
 --     (
---     SELECT @idPaisInc, eliminado, nombre  FROM tabla.pais
+--     SELECT @idPaisInc, eliminado, nombre  FROM minorista.pais
 --     );
  SET  x = x + 1; 
  SET  @idPaisInc = @idPaisInc + 1; 
