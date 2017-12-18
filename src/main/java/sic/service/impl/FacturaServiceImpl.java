a4498f9b644d1ce14d163fb400a6bf685fdc6736
Jose Mayol <mayol.jose.f@gmail.com>
2017-12-18 10:52:08 -0300

    Corrige la comparacion por venta minima en lugar de cantidad minima.

--- src/main/java/sic/service/impl/FacturaServiceImpl.java
+++ src/main/java/sic/service/impl/FacturaServiceImpl.java
@@ -1120,7 +1120,7 @@ public class FacturaServiceImpl implements IFacturaService {
             double cantidad, Long idProducto, double descuento_porcentaje) {
 
         Producto producto = productoService.getProductoPorId(idProducto);
-        if ((movimiento == Movimiento.VENTA || movimiento == Movimiento.PEDIDO) && cantidad < producto.getCantMinima()) {
+        if ((movimiento == Movimiento.VENTA || movimiento == Movimiento.PEDIDO) && cantidad < producto.getVentaMinima()) {
             throw new BusinessServiceException(ResourceBundle.getBundle("Mensajes")
                     .getString("mensaje_producto_cantidad_menor_a_minima"));
         }
