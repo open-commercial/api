298c6a630fdabde324425defdfe05e12634518c2
Jose <mayol.jose.f@gmail.com>
2018-02-27 22:55:06 -0300

    Modifica la cantidad de la verificación de stock luego de dar de alta una NC.

--- src/test/java/sic/integration/CuentaCorrienteIntegrationTest.java
+++ src/test/java/sic/integration/CuentaCorrienteIntegrationTest.java
@@ -367,7 +367,7 @@ public class CuentaCorrienteIntegrationTest {
                 + "&iva105Neto=" + notaCredito.getIva105Neto(), BigDecimal.class));
         restTemplate.postForObject(apiPrefix + "/notas/credito/empresa/1/cliente/1/usuario/1/factura/1?modificarStock=true", notaCredito, NotaCredito.class);
         restTemplate.getForObject(apiPrefix + "/notas/2/reporte", byte[].class);
-        uri = apiPrefix + "/productos/disponibilidad-stock?idProducto=" + productoUno.getId_Producto() + "," + productoDos.getId_Producto() + "&cantidad=5,4";
+        uri = apiPrefix + "/productos/disponibilidad-stock?idProducto=" + productoUno.getId_Producto() + "," + productoDos.getId_Producto() + "&cantidad=10,4";
         Assert.assertTrue(restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<Map<Double, Producto>>() {}).getBody().isEmpty());
         assertTrue("El saldo de la cuenta corriente no es el esperado", 
                 restTemplate.getForObject(apiPrefix + "/cuentas-corrientes/clientes/1/saldo", BigDecimal.class)
