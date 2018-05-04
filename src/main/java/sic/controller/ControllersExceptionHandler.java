a9d7ccc89cb575d489c78190020c83158dbbdca2
Franco Bellucci <belluccifranco@gmail.com>
2018-05-04 15:00:45 -0300

    Corrige el logging para atrapar correctamente las exceptions de hibernate validator

--- src/main/java/sic/controller/ControllersExceptionHandler.java
+++ src/main/java/sic/controller/ControllersExceptionHandler.java
@@ -20,52 +20,48 @@ public class ControllersExceptionHandler {
 
     private String log(Exception ex) {
         String mensaje = ex.getMessage() + "\n(Transaction ID: " + new Date().getTime() + ")";
-        if (ex.getCause() != null) {
-            LOGGER.error(mensaje + " " + ex.getCause().getMessage() + "\n" + Arrays.toString(ex.getStackTrace()));
-        } else {
-            LOGGER.error(mensaje + " " + ResourceBundle.getBundle("Mensajes")
-                    .getString("mensaje_error_sin_causa") + "\n" + Arrays.toString(ex.getStackTrace()));
-        }
+        LOGGER.error(mensaje, ex);
         return mensaje;
-    }
-    
-    @ExceptionHandler(ServiceException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public String handleServiceException(ServiceException ex) {
-        return log(ex);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public String handleConstraintViolationException(ConstraintViolationException ex) {
-        log(ex);
-        String message = "";
+        String mensaje = "";
         for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
-            message = message.concat(violation.getMessage() + "\n");
+            mensaje = mensaje.concat(violation.getMessage() + "\n");
         }
-        return message;
+        mensaje = mensaje.concat("(Transaction ID: " + new Date().getTime() + ")");
+        LOGGER.error(mensaje, ex);
+        return mensaje;
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleServiceException(ServiceException ex) {
+        return this.log(ex);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ResponseBody
     public String handleUnauthorizedException(UnauthorizedException ex) {
-        return log(ex);
+        return this.log(ex);
     }       
     
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public String handleEntityNotFoundException(EntityNotFoundException ex) {        
-        return log(ex);
+    public String handleEntityNotFoundException(EntityNotFoundException ex) {
+        return this.log(ex);
     }
     
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public String handleException(Exception ex) {
-        return log(ex);
+        return this.log(ex);
     }
 }
