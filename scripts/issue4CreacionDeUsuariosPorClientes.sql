cf5b4ac527877d4f0864ba4309bc095a16f56300
Jose <mayol.jose.f@gmail.com>
2018-05-26 04:43:02 -0300

    Modifica el script para que todos los usuarios nuevos tengan el rol de cliente.

--- scripts/issue4CreacionDeUsuariosPorClientes.sql
+++ scripts/issue4CreacionDeUsuariosPorClientes.sql
@@ -1,14 +1,25 @@
 -- ALTER TABLE usuario
 -- ADD columnaTemporal bigint(20); 
--- -- 
+-- -- -- 
 
 -- INSERT INTO usuario(apellido, eliminado, email,
 --   habilitado, nombre, username, password, passwordRecoveryKey, columnaTemporal)
---  SELECT cliente.razonSocial , cliente.eliminado, (SELECT IF(email = "",concat(rand(id_Cliente) * 100000000000000000, "@","globo.com" ), email) AS emailColumn), 
---  false, cliente.razonSocial, rand(id_Cliente) * 100000000000000000, md5("globo123"), 0 , id_Cliente
+--  SELECT cliente.razonSocial , cliente.eliminado, (SELECT IF(email = "",concat(rand(id_Cliente) * 1000000000000000, "@","globo.com" ), email) AS emailColumn), 
+--  false, cliente.razonSocial, rand(id_Cliente) * 1000000000000000, md5("globo123"), 0 , id_Cliente
 --  FROM cliente;
--- -- 
+-- -- -- 
+-- 
+-- SET SQL_SAFE_UPDATES = 0;
+-- SET foreign_key_checks = 0;
+-- SET UNIQUE_CHECKS = 0; 
+-- INSERT INTO rol(id_Usuario, nombre)
+-- SELECT usuario.id_Usuario, "CLIENTE"
+-- FROM usuario;
+-- SET SQL_SAFE_UPDATES = 1;
+-- SET foreign_key_checks = 1;
+-- SET UNIQUE_CHECKS = 1; 
 
+-- 
 -- SET SQL_SAFE_UPDATES=0;
 -- update usuario inner join cliente on cliente.id_Cliente = usuario.columnaTemporal
 -- set cliente.id_Usuario_Credencial = usuario.id_Usuario
@@ -17,3 +28,4 @@
 -- 
 -- ALTER TABLE usuario
 -- DROP COLUMN columnaTemporal; 
+-- 
