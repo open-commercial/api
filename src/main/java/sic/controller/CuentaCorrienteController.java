649cad26c961a771f793f7de139ad14eca418cdf
Franco Bellucci <belluccifranco@gmail.com>
2018-08-13 19:54:30 -0300

    Quita endpoint sin uso de CC. Aplica format

--- src/main/java/sic/controller/CuentaCorrienteController.java
+++ src/main/java/sic/controller/CuentaCorrienteController.java
@@ -2,7 +2,6 @@ package sic.controller;
 
 import java.math.BigDecimal;
 import java.util.ResourceBundle;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -28,11 +27,11 @@ import sic.service.IProveedorService;
 @RestController
 @RequestMapping("/api/v1")
 public class CuentaCorrienteController {
-    
-    private final ICuentaCorrienteService cuentaCorrienteService;
-    private final IProveedorService proveedorService;
-    private final IClienteService clienteService;
-    private final int TAMANIO_PAGINA_DEFAULT = 100;
+
+  private final ICuentaCorrienteService cuentaCorrienteService;
+  private final IProveedorService proveedorService;
+  private final IClienteService clienteService;
+  private static final int TAMANIO_PAGINA_DEFAULT = 50;
 
   @Autowired
   public CuentaCorrienteController(
@@ -44,91 +43,133 @@ public class CuentaCorrienteController {
     this.proveedorService = proveedorService;
   }
 
-    @DeleteMapping("/cuentas-corrientes/{idCuentaCorriente}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @AccesoRolesPermitidos(Rol.ADMINISTRADOR)
-    public void eliminar(@PathVariable Long idCuentaCorriente) {
-        cuentaCorrienteService.eliminar(idCuentaCorriente);
-    }
-    
-    @GetMapping("/cuentas-corrientes/{idCuentaCorriente}")
-    @ResponseStatus(HttpStatus.OK)
-    @AccesoRolesPermitidos({Rol.ADMINISTRADOR, Rol.ENCARGADO, Rol.VENDEDOR, Rol.VIAJANTE, Rol.COMPRADOR})
-    public CuentaCorriente getCuentaCorrientePorId(@PathVariable Long idCuentaCorriente) {
-        return cuentaCorrienteService.getCuentaCorrientePorID(idCuentaCorriente);
-    }
-    
-    @GetMapping("/cuentas-corrientes/clientes/{idCliente}")
-    @ResponseStatus(HttpStatus.OK)
-    @AccesoRolesPermitidos({Rol.ADMINISTRADOR, Rol.ENCARGADO, Rol.VENDEDOR, Rol.VIAJANTE, Rol.COMPRADOR})
-    public CuentaCorrienteCliente getCuentaCorrientePorCliente(@PathVariable Long idCliente) {
-        return cuentaCorrienteService.getCuentaCorrientePorCliente(clienteService.getClientePorId(idCliente));
-    }
-    
-    @GetMapping("/cuentas-corrientes/proveedores/{idProveedor}")
-    @ResponseStatus(HttpStatus.OK)
-    @AccesoRolesPermitidos({Rol.ADMINISTRADOR, Rol.ENCARGADO, Rol.VENDEDOR, Rol.VIAJANTE, Rol.COMPRADOR})
-    public CuentaCorrienteProveedor getCuentaCorrientePorProveedor(@PathVariable Long idProveedor) {
-        return cuentaCorrienteService.getCuentaCorrientePorProveedor(proveedorService.getProveedorPorId(idProveedor));
-    }
-    
-    @GetMapping("/cuentas-corrientes/clientes/{idCliente}/saldo")
-    @ResponseStatus(HttpStatus.OK)
-    @AccesoRolesPermitidos({Rol.ADMINISTRADOR, Rol.ENCARGADO, Rol.VENDEDOR, Rol.VIAJANTE, Rol.COMPRADOR})
-    public BigDecimal getSaldoCuentaCorrienteCliente(@PathVariable long idCliente) {       
-        return cuentaCorrienteService.getCuentaCorrientePorCliente(clienteService.getClientePorId(idCliente)).getSaldo();
-    }
-    
-    @GetMapping("/cuentas-corrientes/proveedores/{idProveedor}/saldo")
-    @ResponseStatus(HttpStatus.OK)
-    @AccesoRolesPermitidos({Rol.ADMINISTRADOR, Rol.ENCARGADO, Rol.VENDEDOR, Rol.VIAJANTE, Rol.COMPRADOR})
-    public BigDecimal getSaldoCuentaCorrienteProveedor(@PathVariable long idProveedor) {       
-        return cuentaCorrienteService.getCuentaCorrientePorProveedor(proveedorService.getProveedorPorId(idProveedor)).getSaldo();
-    }
-    
-    @GetMapping("/cuentas-corrientes/{idCuentaCorriente}/renglones")
-    @ResponseStatus(HttpStatus.OK)
-    @AccesoRolesPermitidos({Rol.ADMINISTRADOR, Rol.ENCARGADO, Rol.VENDEDOR, Rol.VIAJANTE, Rol.COMPRADOR})
-    public Page<RenglonCuentaCorriente> getRenglonesCuentaCorriente(@PathVariable long idCuentaCorriente,
-                                                                    @RequestParam(required = false) Integer pagina,
-                                                                    @RequestParam(required = false) Integer tamanio) {  
-        if (tamanio == null || tamanio <= 0) tamanio = TAMANIO_PAGINA_DEFAULT;
-        if (pagina == null || pagina < 0) pagina = 0;
-        Pageable pageable = new PageRequest(pagina, tamanio);
-        return cuentaCorrienteService.getRenglonesCuentaCorriente(idCuentaCorriente, pageable);
-    }
+  @DeleteMapping("/cuentas-corrientes/{idCuentaCorriente}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @AccesoRolesPermitidos(Rol.ADMINISTRADOR)
+  public void eliminar(@PathVariable Long idCuentaCorriente) {
+    cuentaCorrienteService.eliminar(idCuentaCorriente);
+  }
 
-    @GetMapping("/cuentas-corrientes/clientes/{idCliente}/reporte")
-    @AccesoRolesPermitidos({Rol.ADMINISTRADOR, Rol.ENCARGADO, Rol.VENDEDOR, Rol.VIAJANTE, Rol.COMPRADOR})
-    public ResponseEntity<byte[]> getReporteCuentaCorrienteXls(@PathVariable long idCliente,
-                                                               @RequestParam(required = false) Integer pagina,
-                                                               @RequestParam(required = false) Integer tamanio,
-                                                               @RequestParam(required = false) String formato) {
-        if (tamanio == null || tamanio <= 0) {
-            tamanio = TAMANIO_PAGINA_DEFAULT;
-        }
-        if (pagina == null || pagina < 0) {
-            pagina = 0;
-        }
-        Pageable pageable = new PageRequest(pagina, tamanio);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-        switch (formato) {
-            case "xlsx":
-                headers.setContentType(new MediaType("application", "vnd.ms-excel"));
-                headers.set("Content-Disposition", "attachment; filename=EstadoCuentaCorriente.xlsx");
-                byte[] reporteXls = cuentaCorrienteService.getReporteCuentaCorrienteCliente(cuentaCorrienteService.getCuentaCorrientePorCliente(clienteService.getClientePorId(idCliente)), pageable, formato);
-                headers.setContentLength(reporteXls.length);
-                return new ResponseEntity<>(reporteXls, headers, HttpStatus.OK);
-            case "pdf":
-                headers.setContentType(MediaType.APPLICATION_PDF);
-                headers.add("Content-Disposition", "attachment; filename=EstadoCuentaCorriente.pdf");
-                byte[] reportePDF = cuentaCorrienteService.getReporteCuentaCorrienteCliente(cuentaCorrienteService.getCuentaCorrientePorCliente(clienteService.getClientePorId(idCliente)), pageable, formato);
-                return new ResponseEntity<>(reportePDF, headers, HttpStatus.OK);
-            default:
-                throw new BusinessServiceException(ResourceBundle.getBundle("Mensajes")
-                        .getString("mensaje_formato_no_valido"));
-        }
-    }
+  @GetMapping("/cuentas-corrientes/clientes/{idCliente}")
+  @ResponseStatus(HttpStatus.OK)
+  @AccesoRolesPermitidos({
+    Rol.ADMINISTRADOR,
+    Rol.ENCARGADO,
+    Rol.VENDEDOR,
+    Rol.VIAJANTE,
+    Rol.COMPRADOR
+  })
+  public CuentaCorrienteCliente getCuentaCorrientePorCliente(@PathVariable Long idCliente) {
+    return cuentaCorrienteService.getCuentaCorrientePorCliente(
+        clienteService.getClientePorId(idCliente));
+  }
 
+  @GetMapping("/cuentas-corrientes/proveedores/{idProveedor}")
+  @ResponseStatus(HttpStatus.OK)
+  @AccesoRolesPermitidos({
+    Rol.ADMINISTRADOR,
+    Rol.ENCARGADO,
+    Rol.VENDEDOR,
+    Rol.VIAJANTE,
+    Rol.COMPRADOR
+  })
+  public CuentaCorrienteProveedor getCuentaCorrientePorProveedor(@PathVariable Long idProveedor) {
+    return cuentaCorrienteService.getCuentaCorrientePorProveedor(
+        proveedorService.getProveedorPorId(idProveedor));
+  }
+
+  @GetMapping("/cuentas-corrientes/clientes/{idCliente}/saldo")
+  @ResponseStatus(HttpStatus.OK)
+  @AccesoRolesPermitidos({
+    Rol.ADMINISTRADOR,
+    Rol.ENCARGADO,
+    Rol.VENDEDOR,
+    Rol.VIAJANTE,
+    Rol.COMPRADOR
+  })
+  public BigDecimal getSaldoCuentaCorrienteCliente(@PathVariable long idCliente) {
+    return cuentaCorrienteService
+        .getCuentaCorrientePorCliente(clienteService.getClientePorId(idCliente))
+        .getSaldo();
+  }
+
+  @GetMapping("/cuentas-corrientes/proveedores/{idProveedor}/saldo")
+  @ResponseStatus(HttpStatus.OK)
+  @AccesoRolesPermitidos({
+    Rol.ADMINISTRADOR,
+    Rol.ENCARGADO,
+    Rol.VENDEDOR,
+    Rol.VIAJANTE,
+    Rol.COMPRADOR
+  })
+  public BigDecimal getSaldoCuentaCorrienteProveedor(@PathVariable long idProveedor) {
+    return cuentaCorrienteService
+        .getCuentaCorrientePorProveedor(proveedorService.getProveedorPorId(idProveedor))
+        .getSaldo();
+  }
+
+  @GetMapping("/cuentas-corrientes/{idCuentaCorriente}/renglones")
+  @ResponseStatus(HttpStatus.OK)
+  @AccesoRolesPermitidos({
+    Rol.ADMINISTRADOR,
+    Rol.ENCARGADO,
+    Rol.VENDEDOR,
+    Rol.VIAJANTE,
+    Rol.COMPRADOR
+  })
+  public Page<RenglonCuentaCorriente> getRenglonesCuentaCorriente(
+      @PathVariable long idCuentaCorriente,
+      @RequestParam(required = false) Integer pagina,
+      @RequestParam(required = false) Integer tamanio) {
+    if (tamanio == null || tamanio <= 0) tamanio = TAMANIO_PAGINA_DEFAULT;
+    if (pagina == null || pagina < 0) pagina = 0;
+    Pageable pageable = new PageRequest(pagina, tamanio);
+    return cuentaCorrienteService.getRenglonesCuentaCorriente(idCuentaCorriente, pageable);
+  }
+
+  @GetMapping("/cuentas-corrientes/clientes/{idCliente}/reporte")
+  @AccesoRolesPermitidos({
+    Rol.ADMINISTRADOR,
+    Rol.ENCARGADO,
+    Rol.VENDEDOR,
+    Rol.VIAJANTE,
+    Rol.COMPRADOR
+  })
+  public ResponseEntity<byte[]> getReporteCuentaCorrienteXls(
+      @PathVariable long idCliente,
+      @RequestParam(required = false) Integer pagina,
+      @RequestParam(required = false) Integer tamanio,
+      @RequestParam(required = false) String formato) {
+    if (tamanio == null || tamanio <= 0) tamanio = TAMANIO_PAGINA_DEFAULT;
+    if (pagina == null || pagina < 0) pagina = 0;
+    Pageable pageable = new PageRequest(pagina, tamanio);
+    HttpHeaders headers = new HttpHeaders();
+    headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+    switch (formato) {
+      case "xlsx":
+        headers.setContentType(new MediaType("application", "vnd.ms-excel"));
+        headers.set("Content-Disposition", "attachment; filename=EstadoCuentaCorriente.xlsx");
+        byte[] reporteXls =
+            cuentaCorrienteService.getReporteCuentaCorrienteCliente(
+                cuentaCorrienteService.getCuentaCorrientePorCliente(
+                    clienteService.getClientePorId(idCliente)),
+                pageable,
+                formato);
+        headers.setContentLength(reporteXls.length);
+        return new ResponseEntity<>(reporteXls, headers, HttpStatus.OK);
+      case "pdf":
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.add("Content-Disposition", "attachment; filename=EstadoCuentaCorriente.pdf");
+        byte[] reportePDF =
+            cuentaCorrienteService.getReporteCuentaCorrienteCliente(
+                cuentaCorrienteService.getCuentaCorrientePorCliente(
+                    clienteService.getClientePorId(idCliente)),
+                pageable,
+                formato);
+        return new ResponseEntity<>(reportePDF, headers, HttpStatus.OK);
+      default:
+        throw new BusinessServiceException(
+            ResourceBundle.getBundle("Mensajes").getString("mensaje_formato_no_valido"));
+    }
+  }
 }
