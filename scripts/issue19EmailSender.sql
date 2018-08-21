bc8d0f2cc77b34ff791f95b520777e586517dfde
Jose <mayol.jose.f@gmail.com>
2018-08-21 15:09:15 -0300

    Quita las lineas de comentario del script para la modificación del tipo de dato del campo passwordRecoveryKey y agrega la fecha de expiracion del mismo.

--- scripts/issue19EmailSender.sql
+++ scripts/issue19EmailSender.sql
@@ -1,8 +1,8 @@
--- ALTER TABLE usuario
--- DROP COLUMN passwordRecoveryKey;
--- 
--- ALTER TABLE usuario
--- ADD COLUMN passwordRecoveryKey varchar(255);
+ALTER TABLE usuario
+DROP COLUMN passwordRecoveryKey;
+ 
+ALTER TABLE usuario
+ADD COLUMN passwordRecoveryKey varchar(255);
 
 ALTER TABLE usuario
 ADD COLUMN passwordRecoveryKeyExpireDate datetime not null
