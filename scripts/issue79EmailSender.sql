9d858422d0ae51b6e7ceb442ede85f87c562bbcb
Mayol Jose <mayol.jose.f@gmail.com>
2018-08-25 01:09:53 -0300

    Renombra y modifica el script de EmailSender.

--- scripts/issue79EmailSender.sql
+++ scripts/issue79EmailSender.sql
@@ -5,5 +5,4 @@ ALTER TABLE usuario
 ADD COLUMN passwordRecoveryKey varchar(255);
 
 ALTER TABLE usuario
-ADD COLUMN passwordRecoveryKeyExpirationDate datetime 
-DEFAULT now();
+ADD COLUMN passwordRecoveryKeyExpirationDate datetime;
