91946cc8ca39b8663d4929fe1bb460b612af982e
Franco Bellucci <belluccifranco@gmail.com>
2018-08-29 13:49:15 -0300

    Update totalPedidosRealizadosPorClientes.sql

--- scripts/analytics/totalPedidosRealizadosPorClientes.sql
+++ scripts/analytics/totalPedidosRealizadosPorClientes.sql
@@ -1,5 +1,5 @@
-SELECT cliente.razonSocial, count(*) as cantidad_pedidos FROM pedido
-inner join cliente on pedido.id_Cliente = cliente.id_Cliente
+select cliente.razonSocial, cliente.nombreFantasia, count(*) as cantidad_pedidos 
+from pedido inner join cliente on pedido.id_Cliente = cliente.id_Cliente
 where pedido.id_Empresa = 1
 group by cliente.id_Cliente
 order by cantidad_pedidos desc;
