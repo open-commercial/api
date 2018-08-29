5a4ff7d3e6f104528902f50083d8e7d48a819c83
Jose <mayol.jose.f@gmail.com>
2018-08-29 15:54:22 -0300

    Agrega la quita de los comprobantes eliminados de los resultados.

--- scripts/analytics/totalPedidosRealizadosPorClientes.sql
+++ scripts/analytics/totalPedidosRealizadosPorClientes.sql
@@ -1,5 +1,5 @@
 select cliente.razonSocial, cliente.nombreFantasia, count(*) as cantidad_pedidos 
 from pedido inner join cliente on pedido.id_Cliente = cliente.id_Cliente
-where pedido.id_Empresa = 1
+where pedido.id_Empresa = 1 and pedido.eliminado = false
 group by cliente.id_Cliente
 order by cantidad_pedidos desc;
--- scripts/analytics/totalVentasPorCliente.sql
+++ scripts/analytics/totalVentasPorCliente.sql
@@ -1,6 +1,5 @@
-select cliente.razonSocial, cliente.nombreFantasia, (factura.total) as monto_facturado 
-from factura inner join facturaventa on factura.id_Factura = facturaventa.id_Factura
-inner join cliente on facturaventa.id_Cliente = cliente.id_Cliente
-where factura.id_Empresa = 1
-group by cliente.id_Cliente
-order by monto_facturado desc;
+select cliente.razonSocial as razonSocialTablaFacturas, sum(factura.total) as montoFacturado from cliente
+inner join facturaventa on facturaventa.id_Cliente = cliente.id_Cliente
+inner join factura on factura.id_Factura = facturaventa.id_Factura
+where cliente.id_Empresa = 1 and factura.eliminada = false
+group by cliente.id_Cliente;
