9c9478e93b2c5bdc3cecd3928b4f527fe0458cef
Franco Bellucci <belluccifranco@gmail.com>
2018-08-29 19:45:37 -0300

    Update totalVentasPorCliente.sql

--- scripts/analytics/totalVentasPorCliente.sql
+++ scripts/analytics/totalVentasPorCliente.sql
@@ -2,4 +2,5 @@ select cliente.razonSocial as razonSocialTablaFacturas, sum(factura.total) as mo
 inner join facturaventa on facturaventa.id_Cliente = cliente.id_Cliente
 inner join factura on factura.id_Factura = facturaventa.id_Factura
 where cliente.id_Empresa = 1 and factura.eliminada = false
-group by cliente.id_Cliente;
+group by cliente.id_Cliente
+order by montoFacturado desc;
