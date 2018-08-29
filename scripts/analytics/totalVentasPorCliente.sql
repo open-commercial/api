397f9d5e05e02f7b678a1b80fd4f4facb54d8afd
Franco Bellucci <belluccifranco@gmail.com>
2018-08-29 13:57:37 -0300

    Update totalVentasPorCliente.sql

--- scripts/analytics/totalVentasPorCliente.sql
+++ scripts/analytics/totalVentasPorCliente.sql
@@ -1,5 +1,6 @@
-SELECT cliente.razonSocial, sum(factura.total) as monto_facturado FROM factura 
-inner join facturaventa on factura.id_Factura = facturaventa.id_Factura
+select cliente.razonSocial, cliente.nombreFantasia, (factura.total) as monto_facturado 
+from factura inner join facturaventa on factura.id_Factura = facturaventa.id_Factura
 inner join cliente on facturaventa.id_Cliente = cliente.id_Cliente
 where factura.id_Empresa = 1
-group by cliente.id_Cliente order by monto_facturado desc;
+group by cliente.id_Cliente
+order by monto_facturado desc;
