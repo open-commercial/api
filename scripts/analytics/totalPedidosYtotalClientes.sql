8ce0780222432b9a4fcdcabd0f094f1d93581616
Mayol Jose <mayol.jose.f@gmail.com>
2018-08-29 14:06:22 -0300

    Agrega script que lista por cliente la cantidad de pedidos realizados y el total de acumulado de facturas.

---
+++ scripts/analytics/totalPedidosYtotalClientes.sql
@@ -0,0 +1,12 @@
+select razonSocialTablaFacturas, TRUNCATE(monto, 2) AS MONTO, cantPedidos 
+from (select cliente.id_cliente as idClienteTablaFacturas, cliente.razonSocial as razonSocialTablaFacturas, sum(factura.total) as monto from cliente
+inner join facturaventa on facturaventa.id_Cliente = cliente.id_Cliente
+inner join factura on factura.id_Factura = facturaventa.id_Factura
+where cliente.id_Empresa = 1
+group by cliente.id_Cliente) as tablaFacturas 
+inner join (select cliente.id_Cliente as idClienteTablaPedidos, cliente.razonSocial as razonSocialTablaPedidos, count(*) as cantPedidos
+from pedido
+inner join cliente on pedido.id_Cliente = cliente.id_Cliente
+where cliente.id_Empresa = 1
+group by cliente.id_Cliente) as tablaPedidos
+on tablaFacturas.idClienteTablaFacturas = tablaPedidos.idClienteTablaPedidos;
