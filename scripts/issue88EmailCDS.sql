e635f3f1adade821d82aef42d8b4d4a488dd2e73
Franco Bellucci <belluccifranco@gmail.com>
2018-09-08 03:48:32 -0300

    Modifica el script de cdf

--- scripts/issue88EmailCDS.sql
+++ scripts/issue88EmailCDS.sql
@@ -2,7 +2,7 @@ ALTER TABLE configuraciondelsistema
 ADD emailSenderHabilitado bit(1) default false after signTokenWSAA;
 
 ALTER TABLE configuraciondelsistema
-ADD emailUsername varchar(255) after EmailSenderHabilitado;
+ADD emailUsername varchar(255) after emailSenderHabilitado;
 
 ALTER TABLE configuraciondelsistema
-ADD emailPassword varchar(255) after EmailUsername;
+ADD emailPassword varchar(255) after emailUsername;
