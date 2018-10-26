2bae0e8d85d116b016ccd9e22599cafff62e4106
Franco Bellucci <belluccifranco@gmail.com>
2018-10-26 01:38:37 -0300

    Corrige el script para quitar la column venta minima

--- scripts/issue158Bulto.sql
+++ scripts/issue158Bulto.sql
@@ -1,6 +1,6 @@
-SET SQL_SAFE_UPDATES=0;
-alter table producto add column bulto decimal(25,15) after publico;
+SET SQL_SAFE_UPDATES = 0;
+alter table producto add column bulto decimal(25,15) after cantidad;
 update producto
 set producto.bulto = 1;
 alter table producto drop column ventaMinima;
-SET SQL_SAFE_UPDATES=0;
+SET SQL_SAFE_UPDATES = 1;
