756af20921983d646b2add392e89527295358430
Franco Bellucci <belluccifranco@gmail.com>
2018-11-19 21:07:42 -0300

    Quita el atributo precioBonificado de la vista de producto public

--- src/main/java/sic/modelo/Producto.java
+++ src/main/java/sic/modelo/Producto.java
@@ -50,7 +50,6 @@ public class Producto implements Serializable {
   private boolean hayStock;
 
   @Transient
-  @JsonView(Views.Public.class)
   private BigDecimal precioBonificado;
 
   @Column(precision = 25, scale = 15)
