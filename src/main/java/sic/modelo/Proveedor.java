330d6d37fe128da1df81c7284b7e6a140819c9c5
Jose Mayol <mayol.jose.f@gmail.com>
2019-01-22 00:50:56 -0300

    Modifica el model de Proveedor.

--- src/main/java/sic/modelo/Proveedor.java
+++ src/main/java/sic/modelo/Proveedor.java
@@ -75,13 +75,33 @@ public class Proveedor implements Serializable {
     return empresa.getId_Empresa();
   }
 
-  @JsonGetter("nombreLocalidad")
-  public String getNombreLocalidad() {
-    return localidad.getNombre();
+  @JsonGetter("idLocalidad")
+  public Long getIdLocalidad() {
+    return (localidad != null) ? localidad.getId_Localidad() : null;
   }
 
-  @JsonGetter("idLocalidad")
-  public long getIdLocalidad() {
-    return localidad.getId_Localidad();
+  @JsonGetter("nombreLocalidad")
+  public String getNombreLocalidad() {
+    return (localidad != null) ? localidad.getNombre() : null;
+  }
+
+  @JsonGetter("idProvincia")
+  public Long getIdProvincia() {
+    return (localidad != null) ? localidad.getProvincia().getId_Provincia() : null;
+  }
+
+  @JsonGetter("nombreProvincia")
+  public String getNombreProvincia() {
+    return (localidad != null) ? localidad.getProvincia().getNombre() : null;
+  }
+
+  @JsonGetter("idPais")
+  public Long getIdPais() {
+    return (localidad != null) ? localidad.getProvincia().getPais().getId_Pais() : null;
+  }
+
+  @JsonGetter("nombrePais")
+  public String getNombrePais() {
+    return (localidad != null) ? localidad.getProvincia().getPais().getNombre() : null;
   }
 }
