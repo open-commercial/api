8dbdae7836108e3b9aa568161c4435a45e7a637e
Franco Bellucci <belluccifranco@gmail.com>
2019-02-11 21:45:19 -0300

    Agrega la logica necesaria para procesar el totalizador de recibos

---
+++ src/main/java/sic/repository/custom/ReciboRepositoryImpl.java
@@ -0,0 +1,28 @@
+package sic.repository.custom;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import sic.modelo.QRecibo;
+import sic.repository.ReciboRepositoryCustom;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+
+public class ReciboRepositoryImpl implements ReciboRepositoryCustom {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  @Override
+  public BigDecimal getTotalRecibos(BooleanBuilder builder) {
+    QRecibo qRecibo = QRecibo.recibo;
+    JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    return queryFactory
+      .select(qRecibo.monto.sum())
+      .from(qRecibo)
+      .where(builder)
+      .fetch()
+      .get(0);
+  }
+}
