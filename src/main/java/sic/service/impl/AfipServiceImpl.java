4978eb5e0e9da5d3819f9b9361a9b249a762caf5
Mayol Jose <mayol.jose.f@gmail.com>
2019-06-14 21:24:26 -0300

    Agrega cambio al detalle del comprobante afip para no informar iva si el importe de iva es 0.

--- src/main/java/sic/service/impl/AfipServiceImpl.java
+++ src/main/java/sic/service/impl/AfipServiceImpl.java
@@ -461,6 +461,9 @@ public class AfipServiceImpl implements IAfipService {
             .add(comprobante.getIva21neto())
             .setScale(2, RoundingMode.HALF_UP)
             .doubleValue());
+    if (detalle.getImpIVA() == 0) {
+      detalle.setIva(null);
+    }
     // Importe neto gravado. Debe ser menor o igual a Importe total y no
     // puede ser menor a cero. Para comprobantes tipo C este campo
     // corresponde al Importe del Sub Total
