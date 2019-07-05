fb0c88d3c5a3eaad836508ead44556340a0a0d8b
Jose Mayol <mayol.jose.f@gmail.com>
2019-07-05 19:58:23 -0300

    Agrega metodos para lanzar exceptions con mensajes propios.

--- src/main/java/sic/service/impl/PagoMercadoPagoServiceImpl.java
+++ src/main/java/sic/service/impl/PagoMercadoPagoServiceImpl.java
@@ -16,6 +16,7 @@ import sic.modelo.dto.PagoMercadoPagoDTO;
 import sic.service.*;
 
 import java.math.BigDecimal;
+import java.text.MessageFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -77,6 +78,7 @@ public class PagoMercadoPagoServiceImpl implements IPagoMercadoPagoService {
           .setTransactionAmount(pagoMercadoPagoDTO.getMonto())
           .setDescription("Pago a Globo de Oro - Debito")
           .setPaymentMethodId(pagoMercadoPagoDTO.getPaymentMethodId())
+          .setBinaryMode(true)
           .setPayer(payer);
     } else {
       throw new BusinessServiceException(RESOURCE_BUNDLE.getString("mensaje_pago_no_soportado"));
@@ -96,10 +98,11 @@ public class PagoMercadoPagoServiceImpl implements IPagoMercadoPagoService {
         nuevoRecibo.setIdPagoMercadoPago(payment.getId());
         nuevoRecibo = reciboService.guardar(nuevoRecibo);
       } else {
-        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("mensaje_pago_no_aprobado"));
+        this.lanzarExcepcionSegunDetalleDePayment(payment);
       }
-    } catch (MPException e) {
-      logger.error(e.toString());
+    } catch (MPException exception) {
+      logger.error(exception.toString());
+      this.lanzarBussinesExceptionPorExceptionMercadoPago(exception);
     }
     return nuevoRecibo;
   }
@@ -120,5 +123,203 @@ public class PagoMercadoPagoServiceImpl implements IPagoMercadoPagoService {
       logger.error("El pago no se pudo recuperar.");
     }
     return pagoRecuperado;
+  }
+
+  private void lanzarExcepcionSegunDetalleDePayment(Payment payment) {
+    switch (payment.getStatusDetail()) {
+      case "pending_contingency":
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("pending_contingency"));
+      case "pending_review_manual":
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("pending_review_manual"));
+      case "cc_rejected_bad_filled_card_number":
+        throw new BusinessServiceException(
+            RESOURCE_BUNDLE.getString("cc_rejected_bad_filled_card_number"));
+      case "cc_rejected_bad_filled_date":
+        throw new BusinessServiceException(
+            RESOURCE_BUNDLE.getString("cc_rejected_bad_filled_date"));
+      case "cc_rejected_bad_filled_other":
+        throw new BusinessServiceException(
+            RESOURCE_BUNDLE.getString("cc_rejected_bad_filled_other"));
+      case "cc_rejected_bad_filled_security_code":
+        throw new BusinessServiceException(
+            RESOURCE_BUNDLE.getString("cc_rejected_bad_filled_security_code"));
+      case "cc_rejected_blacklist":
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("cc_rejected_blacklist"));
+      case "cc_rejected_call_for_authorize":
+        throw new BusinessServiceException(
+            RESOURCE_BUNDLE.getString("cc_rejected_call_for_authorize"));
+      case "cc_rejected_card_disabled":
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("cc_rejected_card_disabled"));
+      case "cc_rejected_card_error":
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("cc_rejected_card_error"));
+      case "cc_rejected_duplicated_payment":
+        throw new BusinessServiceException(
+            RESOURCE_BUNDLE.getString("cc_rejected_duplicated_payment"));
+      case "cc_rejected_high_risk":
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("cc_rejected_high_risk"));
+      case "cc_rejected_insufficient_amount":
+        throw new BusinessServiceException(
+            RESOURCE_BUNDLE.getString("cc_rejected_insufficient_amount"));
+      case "cc_rejected_invalid_installments":
+        throw new BusinessServiceException(
+            MessageFormat.format(
+                RESOURCE_BUNDLE.getString("cc_rejected_invalid_installments"),
+                payment.getInstallments()));
+      case "cc_rejected_max_attempts":
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("cc_rejected_max_attempts"));
+      default:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("cc_rejected_other_reason"));
+    }
+  }
+
+  private void lanzarBussinesExceptionPorExceptionMercadoPago(MPException exception) {
+    switch (exception.getStatusCode()) {
+      case 1:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_1"));
+      case 3:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3"));
+      case 5:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_5"));
+      case 1000:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_1000"));
+      case 1001:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_1001"));
+      case 2001:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_2001"));
+      case 2004:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_2004"));
+      case 2002:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_2002"));
+      case 2006:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_2006"));
+      case 2007:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_2007"));
+      case 2009:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_2009"));
+      case 2060:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_2060"));
+      case 3000:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3000"));
+      case 3001:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3001"));
+      case 3003:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3003"));
+      case 3004:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3004"));
+      case 3005:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3005"));
+      case 3006:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3006"));
+      case 3007:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3007"));
+      case 3008:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3008"));
+      case 3009:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3009"));
+      case 3010:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3010"));
+      case 3011:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3011"));
+      case 3012:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3012"));
+      case 3013:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3013"));
+      case 3014:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3014"));
+      case 3015:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3015"));
+      case 3016:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3016"));
+      case 3017:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3017"));
+      case 3018:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3018"));
+      case 3019:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3019"));
+      case 3020:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3020"));
+      case 3021:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3021"));
+      case 3022:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3022"));
+      case 3023:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3023"));
+      case 3024:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3024"));
+      case 3025:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3025"));
+      case 3026:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3026"));
+      case 3027:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3027"));
+      case 3028:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3028"));
+      case 3029:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3029"));
+      case 3030:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_3030"));
+      case 4000:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4000"));
+      case 4001:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4001"));
+      case 4002:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4002"));
+      case 4003:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4003"));
+      case 4004:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4004"));
+      case 4005:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4005"));
+      case 4006:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4006"));
+      case 4007:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4007"));
+      case 4012:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4012"));
+      case 4013:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4013"));
+      case 4015:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4015"));
+      case 4016:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4016"));
+      case 4017:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4017"));
+      case 4018:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4018"));
+      case 4019:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4019"));
+      case 4020:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4020"));
+      case 4021:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4021"));
+      case 4022:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4022"));
+      case 4023:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4023"));
+      case 4024:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4024"));
+      case 4025:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4025"));
+      case 4026:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4026"));
+      case 4027:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4027"));
+      case 4028:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4028"));
+      case 4029:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4029"));
+      case 4037:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4037"));
+      case 4038:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4038"));
+      case 4039:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4039"));
+      case 4050:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4050"));
+      case 4051:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_4051"));
+      default:
+        throw new BusinessServiceException(RESOURCE_BUNDLE.getString("error_1"));
+    }
   }
 }
