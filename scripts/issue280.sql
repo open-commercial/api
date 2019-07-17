93d8173ae3fe0378ecdfc452879e1dc2868e1aba
Jose Mayol <mayol.jose.f@gmail.com>
2019-07-17 01:11:20 -0300

    Agrega al script instrucciones para guardar en el concepto de los gastos y recibos, la forma de pago anterior que poseían antes de cambiarlas.

--- scripts/issue280.sql
+++ scripts/issue280.sql
@@ -8,11 +8,29 @@ INSERT INTO formadepago (afectaCaja, eliminada, nombre, predeterminado)
 SET SQL_SAFE_UPDATES = 0;
  SET FOREIGN_KEY_CHECKS = 0;
 
+UPDATE gasto SET gasto.concepto = CONCAT(gasto.concepto," Forma de pago anterior: Cheque Globo Distribuciones")
+where id_FormaDePago = 19;
+UPDATE gasto SET gasto.concepto = CONCAT(gasto.concepto, " Forma de pago anterior: Cheque Calvano Mario R")
+where id_FormaDePago = 20;
+UPDATE gasto SET gasto.concepto = CONCAT(gasto.concepto, " Forma de pago anterior: Cheque para Pagos")
+where id_FormaDePago = 51;
+UPDATE gasto SET gasto.concepto = CONCAT(gasto.concepto, " Forma de pago anterior: Cheque")
+where  id_FormaDePago = 52;
+
 UPDATE gasto SET id_FormaDePago = 39 where id_FormaDePago = 53;
 UPDATE gasto SET id_FormaDePago = 16 where id_FormaDePago = 37;
 UPDATE gasto SET id_FormaDePago = 15 where id_FormaDePago = 55;
 UPDATE gasto SET id_FormaDePago = 61 
 where id_FormaDePago = 19 or id_FormaDePago = 20 or id_FormaDePago = 51 or id_FormaDePago = 52;
+
+UPDATE recibo SET recibo.concepto = CONCAT(recibo.concepto," Forma de pago anterior: Cheque Globo Distribuciones")
+where id_FormaDePago = 19;
+UPDATE recibo SET recibo.concepto = CONCAT(recibo.concepto, " Forma de pago anterior: Cheque Calvano Mario R")
+where id_FormaDePago = 20;
+UPDATE recibo SET recibo.concepto = CONCAT(recibo.concepto, " Forma de pago anterior: Cheque para Pagos")
+where id_FormaDePago = 51;
+UPDATE recibo SET recibo.concepto = CONCAT(recibo.concepto, " Forma de pago anterior: Cheque")
+where  id_FormaDePago = 52;
 
 UPDATE recibo SET id_FormaDePago = 39 where id_FormaDePago = 53;
 UPDATE recibo SET id_FormaDePago = 16 where id_FormaDePago = 37;
