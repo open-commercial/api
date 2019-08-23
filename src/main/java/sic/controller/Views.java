8c957a6482c565e28f9eae6c9dd305966b8e9344
Franco Bellucci <belluccifranco@gmail.com>
2019-08-23 18:59:51 -0300

    Quita codigo sin uso

--- src/main/java/sic/controller/Views.java
+++ src/main/java/sic/controller/Views.java
@@ -1,21 +1,8 @@
 package sic.controller;
 
-import sic.modelo.Rol;
-import java.util.EnumMap;
-import java.util.Map;
-
 public class Views {
 
-  public static final Map<Rol, Class> MAPPING = new EnumMap<>(Rol.class);
-
   // https://www.baeldung.com/spring-security-role-filter-json
-  static {
-    MAPPING.put(Rol.COMPRADOR, Comprador.class);
-    MAPPING.put(Rol.VIAJANTE, Viajante.class);
-    MAPPING.put(Rol.VENDEDOR, Vendedor.class);
-    MAPPING.put(Rol.ENCARGADO, Encargado.class);
-    MAPPING.put(Rol.ADMINISTRADOR, Administrador.class);
-  }
 
   public static class Comprador {}
   public static class Viajante extends Comprador {}
