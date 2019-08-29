4201c4ff0f22a00f5f4d2d598c6e30e60dee0bf6
Jose Mayol <mayol.jose.f@gmail.com>
2019-08-28 23:39:31 -0300

    Vuelve atras el script.

--- scripts/issue86CAEenRenglonesCC.sql
+++ scripts/issue86CAEenRenglonesCC.sql
@@ -5,7 +5,7 @@ SET SQL_SAFE_UPDATES = 0;
 UPDATE 
     rengloncuentacorriente inner join factura on rengloncuentacorriente.id_Factura = factura.id_Factura
 SET 
-	rengloncuentacorriente.cae = factura.CAE
+	rengloncuentacorriente.CAE = factura.CAE
 WHERE factura.CAE > 0;
 UPDATE 
     rengloncuentacorriente inner join nota on rengloncuentacorriente.idNota = nota.idNota
