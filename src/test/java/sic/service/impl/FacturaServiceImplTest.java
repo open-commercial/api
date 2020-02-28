c34e5731b34e913ee4232c2d64f2ff313afc8d0a
Mayol Jose <mayol.jose.f@gmail.com>
2020-02-28 20:33:19 -0300

    Agrega test para los calculos de arrays.

--- src/test/java/sic/service/impl/FacturaServiceImplTest.java
+++ src/test/java/sic/service/impl/FacturaServiceImplTest.java
@@ -792,4 +792,45 @@ class FacturaServiceImplTest {
         facturaServiceImpl.calcularPrecioUnitario(Movimiento.VENTA, TipoDeComprobante.FACTURA_C, producto)
             .compareTo(new BigDecimal("242")));
   }
+
+  @Test
+  void shouldGetArrayDeIdProductoParaFactura() {
+    long[] arrayEsperado = {1L, 2L, 3L};
+    List<NuevoRenglonFacturaDTO> nuevosRenglonsFactura = new ArrayList<>();
+    nuevosRenglonsFactura.add(NuevoRenglonFacturaDTO.builder().idProducto(1L).build());
+    nuevosRenglonsFactura.add(NuevoRenglonFacturaDTO.builder().idProducto(2L).build());
+    nuevosRenglonsFactura.add(NuevoRenglonFacturaDTO.builder().idProducto(3L).build());
+    assertArrayEquals(
+            arrayEsperado, CalculosComprobante.getArrayDeIdProductoParaFactura(nuevosRenglonsFactura));
+  }
+
+  @Test
+  void shouldGetArrayDeCantidadesParaFactura() {
+    BigDecimal[] arrayEsperado = {new BigDecimal("10"), new BigDecimal("20"), new BigDecimal("5")};
+    List<NuevoRenglonFacturaDTO> nuevosRenglonsFactura = new ArrayList<>();
+    nuevosRenglonsFactura.add(
+            NuevoRenglonFacturaDTO.builder().cantidad(new BigDecimal("10")).build());
+    nuevosRenglonsFactura.add(
+            NuevoRenglonFacturaDTO.builder().cantidad(new BigDecimal("20")).build());
+    nuevosRenglonsFactura.add(
+            NuevoRenglonFacturaDTO.builder().cantidad(new BigDecimal("5")).build());
+    assertArrayEquals(
+            arrayEsperado,
+            CalculosComprobante.getArrayDeCantidadesProductoParaFactura(nuevosRenglonsFactura));
+  }
+
+  @Test
+  void shouldGetArrayDeBonificacionesDeRenglonParaFactura() {
+    BigDecimal[] arrayEsperado = {new BigDecimal("5"), new BigDecimal("2"), new BigDecimal("12")};
+    List<NuevoRenglonFacturaDTO> nuevosRenglonsFactura = new ArrayList<>();
+    nuevosRenglonsFactura.add(
+            NuevoRenglonFacturaDTO.builder().bonificacion(new BigDecimal("5")).build());
+    nuevosRenglonsFactura.add(
+            NuevoRenglonFacturaDTO.builder().bonificacion(new BigDecimal("2")).build());
+    nuevosRenglonsFactura.add(
+            NuevoRenglonFacturaDTO.builder().bonificacion(new BigDecimal("12")).build());
+    assertArrayEquals(
+            arrayEsperado,
+            CalculosComprobante.getArrayDeBonificacionesParaFactura(nuevosRenglonsFactura));
+  }
 }
