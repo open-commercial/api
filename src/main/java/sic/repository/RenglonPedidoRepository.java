d45decf0703ef634b0563d4749f490adb4da272a
Mayol Jose <mayol.jose.f@gmail.com>
2020-03-12 19:03:08 -0300

    HOTFIX Corrige el recupero de renglones de pedido ordenados por idRenglonPedido para evitar los eliminados.

--- src/main/java/sic/repository/RenglonPedidoRepository.java
+++ src/main/java/sic/repository/RenglonPedidoRepository.java
@@ -9,9 +9,10 @@ import java.util.List;
 
 public interface RenglonPedidoRepository extends PagingAndSortingRepository<RenglonPedido, Long> {
 
-    @Query("SELECT rp FROM Pedido p INNER JOIN p.renglones rp"
-            + " WHERE p.idPedido = :idPedido AND p.eliminado = false order by rp.idRenglonPedido asc")
-    List<RenglonPedido> findByIdPedidoOrderByIdRenglonPedido(@Param("idPedido") long idPedido);
+  @Query(
+      "SELECT rp FROM Pedido p INNER JOIN p.renglones rp INNER JOIN Producto producto on rp.idProductoItem = producto.idProducto"
+          + " WHERE p.idPedido = :idPedido AND p.eliminado = false AND producto.eliminado = false order by rp.idRenglonPedido asc")
+  List<RenglonPedido> findByIdPedidoOrderByIdRenglonPedido(@Param("idPedido") long idPedido);
 
     @Query("SELECT rp FROM Pedido p INNER JOIN p.renglones rp"
       + " WHERE p.idPedido = :idPedido AND p.eliminado = false order by rp.idProductoItem asc")
