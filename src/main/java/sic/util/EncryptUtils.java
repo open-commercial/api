029ab2f2dc4e7efb32209a1506586e9631448207
Franco Bellucci <belluccifranco@gmail.com>
2020-03-26 21:20:12 -0300

    Aplica format

--- src/main/java/sic/util/EncryptUtils.java
+++ src/main/java/sic/util/EncryptUtils.java
@@ -13,15 +13,11 @@ public class EncryptUtils {
 
   public static String encryptWhitAES(String valor, String initVectorValue, String key)
       throws GeneralSecurityException {
-
     IvParameterSpec iv = new IvParameterSpec(initVectorValue.getBytes(StandardCharsets.UTF_8));
     SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
-
     Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
     cipher.init(1, skeySpec, iv);
-
     byte[] encrypted = cipher.doFinal(valor.getBytes());
-
     return DatatypeConverter.printBase64Binary(encrypted);
   }
 
@@ -29,10 +25,8 @@ public class EncryptUtils {
       throws GeneralSecurityException {
     IvParameterSpec iv = new IvParameterSpec(initVectorValue.getBytes(StandardCharsets.UTF_8));
     SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
-
     Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
     cipher.init(2, skeySpec, iv);
-
     byte[] original = cipher.doFinal(DatatypeConverter.parseBase64Binary(valorEncriptado));
     return new String(original);
   }
