ee65f062f0d1a7a3e221ae894765eaa03f736473
Mayol Jose <mayol.jose.f@gmail.com>
2020-03-27 19:35:31 -0300

    HOTFIX Corrige el link para la recuperación de contrasenia.

---
+++ src/test/java/sic/service/impl/AppTest.java
@@ -0,0 +1,19 @@
+package sic.service.impl;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class AppTest {
+
+    @Bean
+    public MessageSource messageSourceTest() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
---
+++ src/test/java/sic/service/impl/MessageSourceImplTest.java
@@ -0,0 +1,31 @@
+package sic.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Locale;
+
+import static junit.framework.TestCase.assertFalse;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppTest.class)
+public class MessageSourceImplTest {
+
+  @Autowired private MessageSource messageSourceTest;
+
+  @Test
+  void shouldTestLongSinComaEnElRecuperoDeContrasenia() {
+    assertFalse(
+        messageSourceTest
+            .getMessage(
+                "mensaje_correo_recuperacion",
+                new Object[] {"host", "passwordRecoveryKey", 1000L},
+                Locale.ENGLISH)
+            .contains("1,000"));
+  }
+}
