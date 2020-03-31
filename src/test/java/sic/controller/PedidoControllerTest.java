8f9be1605afc14a57dd3d146f55c46c1e0c938de
Mayol Jose <mayol.jose.f@gmail.com>
2020-03-30 21:25:50 -0300

    Agrega algunos test sobre pedido controller.

---
+++ src/test/java/sic/controller/PedidoControllerTest.java
@@ -0,0 +1,61 @@
+package sic.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import sic.modelo.*;
+import sic.modelo.dto.NuevoRenglonPedidoDTO;
+import sic.modelo.dto.NuevosResultadosComprobanteDTO;
+import sic.service.impl.PedidoServiceImpl;
+import sic.service.impl.ReciboServiceImpl;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class PedidoControllerTest {
+
+  @Mock private PedidoServiceImpl pedidoService;
+
+  @InjectMocks private PedidoController pedidoController;
+
+  @Test
+  void shouldGetPedidoPorId() {
+    Pedido pedido = new Pedido();
+    pedido.setNroPedido(1L);
+    when(pedidoService.getPedidoNoEliminadoPorId(2L)).thenReturn(pedido);
+    when(pedidoService.calcularTotalActualDePedido(pedido)).thenReturn(pedido);
+    pedidoController.getPedidoPorId(2L);
+  }
+
+  @Test
+  void shouldCalcularRenglonesPedido() {
+    List<NuevoRenglonPedidoDTO> nuevoRenglonPedido = new ArrayList<>();
+    nuevoRenglonPedido.add(
+        NuevoRenglonPedidoDTO.builder().idProductoItem(1L).cantidad(BigDecimal.TEN).build());
+    nuevoRenglonPedido.add(
+        NuevoRenglonPedidoDTO.builder().idProductoItem(2L).cantidad(BigDecimal.ONE).build());
+    List<RenglonPedido> renglones = new ArrayList<>();
+    long[] idsProducto = {1L, 2L};
+    BigDecimal[] cantidades = {BigDecimal.TEN, BigDecimal.ONE};
+    when(pedidoService.calcularRenglonesPedido(idsProducto, cantidades)).thenReturn(renglones);
+    pedidoController.calcularRenglonesPedido(nuevoRenglonPedido);
+  }
+
+  @Test
+  void shouldCalcularResultadosPedido() {
+    NuevosResultadosComprobanteDTO nuevosResultadosComprobanteDTO =
+        NuevosResultadosComprobanteDTO.builder().build();
+    Resultados resultados = new Resultados();
+    when(pedidoService.calcularResultadosPedido(nuevosResultadosComprobanteDTO))
+        .thenReturn(resultados);
+    pedidoController.calcularResultadosPedido(nuevosResultadosComprobanteDTO);
+  }
+}
