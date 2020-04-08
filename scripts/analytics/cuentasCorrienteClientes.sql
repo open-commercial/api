d319f7c9f26c5ea381f3711758ded3ba3a68175e
Franco Bellucci <belluccifranco@gmail.com>
2020-04-07 22:40:44 -0300

    Actualiza script de analytics

--- scripts/analytics/cuentasCorrienteClientes.sql
+++ scripts/analytics/cuentasCorrienteClientes.sql
@@ -1,9 +1,19 @@
+
+-- LISTADO DE CUENTAS CORRIENTE CLIENTE CON INFO COMPLETA DE CLIENTE
+
 select cliente.nroCliente, cliente.idFiscal as 'CUIT o DNI', cliente.nombreFiscal as 'R. Social o Nombre',
     cliente.nombreFantasia, cuentacorriente.saldo, cuentacorriente.fechaUltimoMovimiento,
+    cliente.montoCompraMinima,
     concat(usuario.nombre, ' ', usuario.apellido) as 'Viajante',
-    cliente.contacto, cliente.telefono, cliente.email
-from cuentacorriente inner join cuentacorrientecliente on cuentacorriente.id_cuenta_corriente = cuentacorrientecliente.id_cuenta_corriente
+    cliente.contacto, cliente.telefono, cliente.email,
+    ubicacion.calle, ubicacion.numero, ubicacion.piso, ubicacion.departamento,
+    localidad.nombre as 'localidad', provincia.nombre as 'provincia'
+from cuentacorriente
+    inner join cuentacorrientecliente on cuentacorriente.id_cuenta_corriente = cuentacorrientecliente.id_cuenta_corriente
 	inner join cliente on cliente.id_Cliente = cuentacorrientecliente.id_Cliente
+    left join ubicacion on cliente.idUbicacionFacturacion = ubicacion.idUbicacion
+    left join localidad on localidad.idLocalidad = ubicacion.idLocalidad
+    left join provincia on provincia.idProvincia = localidad.idProvincia
     left join usuario on usuario.id_Usuario = cliente.id_Usuario_Viajante
-where cuentacorriente.eliminada = false and cuentacorriente.id_Empresa = 1
+where cuentacorriente.eliminada = false
 order by cuentacorriente.saldo
