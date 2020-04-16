4a41966f58ec014ba3e6ff6968403d8f755e665c
Mayol Jose <mayol.jose.f@gmail.com>
2020-04-15 21:36:03 -0300

    Cambia el tipo de dato del script para la columna version.

--- scripts/issue336.sql
+++ scripts/issue336.sql
@@ -1,4 +1,4 @@
 ALTER TABLE producto
-ADD version int(11) default 1 after urlImagen;
+ADD version bigint(11) default 1 after urlImagen;
 ALTER TABLE cantidadensucursal
-ADD version int(11) default 1 after estante;
+ADD version bigint(11) default 1 after estante;
