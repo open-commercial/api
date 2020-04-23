725eeb8081983fb46b6dec12989a2ebc1cda5c18
Franco Bellucci <belluccifranco@gmail.com>
2020-04-23 12:25:51 -0300

    Update saldosCuentaCorrienteClientesALaFechaX.sql

--- scripts/analytics/saldosCuentaCorrienteClientesALaFechaX.sql
+++ scripts/analytics/saldosCuentaCorrienteClientesALaFechaX.sql
@@ -6,8 +6,8 @@ select cliente.idFiscal, cliente.nombreFiscal, sum(rengloncuentacorriente.monto)
 from cuentacorriente inner join cuentacorrientecliente on cuentacorriente.id_cuenta_corriente = cuentacorrientecliente.id_cuenta_corriente
 	inner join rengloncuentacorriente on cuentacorriente.id_cuenta_corriente = rengloncuentacorriente.id_cuenta_corriente
 	inner join cliente on cuentacorrientecliente.id_Cliente = cliente.id_Cliente
-where cuentacorriente.id_Empresa = 1 and rengloncuentacorriente.eliminado = false
-	and rengloncuentacorriente.fecha <= '2018-12-31 23:59:59'
+where rengloncuentacorriente.eliminado = false
+	and rengloncuentacorriente.fecha <= '2019-12-31 23:59:59'
     and (rengloncuentacorriente.tipo_comprobante = 'FACTURA_A'
 		or rengloncuentacorriente.tipo_comprobante = 'FACTURA_B'
 		or rengloncuentacorriente.tipo_comprobante = 'FACTURA_C'
