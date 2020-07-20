f4afa57af8428a014a64faf3a1ed2f7857150f35
Mayol Jose <mayol.jose.f@gmail.com>
2020-07-20 20:38:48 -0300

    Agrega método en el service para actualizar multiples localidades. Faltan test.

---
+++ src/main/java/sic/modelo/dto/LocalidadesParaActualizarDTO.java
@@ -0,0 +1,19 @@
+package sic.modelo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LocalidadesParaActualizarDTO {
+
+    private long[] idLocalidad;
+    private BigDecimal costoDeEnvio;
+    private boolean envioGratuito;
+}
