981b065bdf55be120ba872b15b3f4320a28997c4
Franco Bellucci <belluccifranco@gmail.com>
2020-07-30 22:22:43 -0300

    Elimina metodo sin uso de factura repository

--- src/main/java/sic/repository/FacturaRepository.java
+++ src/main/java/sic/repository/FacturaRepository.java
@@ -1,12 +1,6 @@
 package sic.repository;
 
-import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import sic.modelo.Factura;
-import sic.modelo.Pedido;
 
-public interface FacturaRepository<T extends Factura> extends PagingAndSortingRepository<T, Long> {
-    
-    List<Factura> findAllByPedidoAndEliminada(Pedido pedido, boolean eliminada);
-    
-}
+public interface FacturaRepository<T extends Factura> extends PagingAndSortingRepository<T, Long> {}
