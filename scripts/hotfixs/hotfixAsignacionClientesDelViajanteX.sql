56e123e67b65e71e6cb97c3500a1b3ec3ea521cf
Mayol Jose <mayol.jose.f@gmail.com>
2021-01-07 23:44:26 -0300

    Agrega script de hotfix para asignar viajante a cliente

---
+++ scripts/hotfixs/hotfixAsignacionClientesDelViajanteX.sql
@@ -0,0 +1,93 @@
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 45;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 46;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 47;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 48;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 159;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 181;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 182;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 184;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 187;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 193;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 196;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 197;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 203;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 205;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 206;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 215;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 219;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 243;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 247;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 252;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 254;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 258;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 259;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 269;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 270;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 291;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 299;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 303;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 304;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 305;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 325;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 327;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 340;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 361;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 370;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 373;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 382;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 386;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 403;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 408;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 417;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 421;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1059;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1063;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1074;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1077;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1078;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1081;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1084;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1100;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1102;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1105;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1118;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1128;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1131;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1154;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1155;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1156;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1180;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1190;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1192;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1220;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1222;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1580;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1581;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1642;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1730;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1731;
+update cliente set cliente.id_Usuario_Viajante = 33 where cliente.id_Cliente = 1746;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
