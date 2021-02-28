04afdccfb2ff02a8b9ffcee3829e3eea69dd30bf
Mayol Jose <mayol.jose.f@gmail.com>
2021-02-28 00:19:32 -0300

    Agrega al script el fill de cantidad reservada

--- scripts/issues/issue415v2.sql
+++ scripts/issues/issue415v2.sql
@@ -1,10 +1,5 @@
 ALTER TABLE producto
 ADD cantidadReservada decimal(25,15) DEFAULT 0 AFTER cantidadTotalEnSucursales; 
-   
-SELECT renglonpedido.idProductoItem as id, sum(renglonpedido.cantidad) as cantidadReservada from pedido inner join renglonpedido 
-on pedido.id_Pedido = renglonpedido.id_Pedido 
-WHERE pedido.estado = "ABIERTO"
-group by renglonpedido.idProductoItem;    
 
 SET SQL_SAFE_UPDATES = 0;
 UPDATE
@@ -18,4 +13,9 @@ group by renglonpedido.idProductoItem
         m.id = t.idProducto
 SET
     t.cantidadReservada = m.cantidadReservada;
+
+UPDATE producto
+SET cantidadReservada = 0
+WHERE cantidadReservada is null;
+
 SET SQL_SAFE_UPDATES = 1;
