fa819813d683217859d164ecde11c7189edf71c9
Mayol Jose <mayol.jose.f@gmail.com>
2021-03-22 07:41:13 -0300

    HOTFIX Agrega al excludePathPatterns la uri de recomendados con el agregado de sucursales

--- src/main/java/sic/config/AppWebMvc.java
+++ src/main/java/sic/config/AppWebMvc.java
@@ -21,7 +21,7 @@ public class AppWebMvc implements WebMvcConfigurer {
         .excludePathPatterns("/api/*/pagos/mercado-pago/notificacion")
         .excludePathPatterns("/api/*/productos/*/sucursales/*")
         .excludePathPatterns("/api/*/productos/busqueda/criteria/sucursales/*")
-        .excludePathPatterns("/api/*/productos/*/recomendados")
+        .excludePathPatterns("/api/*/productos/*/sucursales/*/recomendados")
         .excludePathPatterns("/api/*/rubros/*")
         .excludePathPatterns("/api/*/rubros");
   }
