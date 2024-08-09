496d4111043ff0faadc4981bda6a352427fe1af7
Franco Bellucci <belluccifranco@gmail.com>
2024-08-09 18:10:26 +0200

    Add new methods in the controller for Sales

--- scripts/analytics/montosDeFacturacionPorCliente.sql
+++ scripts/analytics/montosDeFacturacionPorCliente.sql
@@ -1,6 +1,7 @@
 -- Monto de facturas por clientes en un determinado año.
-select cliente.nroCliente, cliente.nombreFiscal, cliente.nombreFantasia, SUM(factura.total) as totalFacturas from factura inner join facturaventa on factura.id_Factura = facturaventa.id_Factura
-inner join cliente on facturaventa.id_Cliente = cliente.id_Cliente
+select cliente.nroCliente, cliente.nombreFiscal, cliente.nombreFantasia, SUM(factura.total) as totalFacturas
+from factura inner join facturaventa on factura.id_Factura = facturaventa.id_Factura
+    inner join cliente on facturaventa.id_Cliente = cliente.id_Cliente
 where year(factura.fecha) = "2019" and factura.eliminada = false and cliente.eliminado = false
 group by cliente.id_Cliente
 order by totalFacturas desc;
