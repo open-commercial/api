ba9f05318a31ec6939ae37044e58ecf50549a2ea
Franco Bellucci <belluccifranco@gmail.com>
2026-02-13 21:54:35 +0100

    Update SQL query for top sold products

--- scripts/analytics/topProductosMasVendidos.sql
+++ scripts/analytics/topProductosMasVendidos.sql
@@ -1,12 +1,12 @@
 SELECT renglonfactura.codigoItem, renglonfactura.descripcionItem, medida.nombre as 'medida',
-SUM(renglonfactura.cantidad) AS 'cantidad', SUM(renglonfactura.importe) as 'importe'
-
+SUM(renglonfactura.cantidad) AS 'cantidad', 
+ROUND(SUM(renglonfactura.importe),2) as 'importe'
+  
 FROM factura INNER JOIN facturaventa ON factura.id_Factura = facturaventa.id_Factura
 INNER JOIN renglonfactura ON renglonfactura.id_Factura = facturaventa.id_Factura
-INNER JOIN producto ON producto.idProducto = renglonfactura.id_ProductoItem
+INNER JOIN producto ON producto.idProducto = renglonfactura.idProductoItem
 INNER JOIN medida ON medida.id_Medida = producto.id_Medida
-
-WHERE factura.eliminada = false AND factura.id_Empresa = 1
+  
+WHERE factura.eliminada = false AND factura.idSucursal = 1
 GROUP BY renglonfactura.codigoItem, renglonfactura.descripcionItem, medida.nombre
-ORDER BY cantidad desc
-LIMIT 0,2000;
+ORDER BY importe desc
